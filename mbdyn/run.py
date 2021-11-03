86680e22002723b46736b9f67dc54493b21ada1f
JulianSchl <83708903+JulianSchl@users.noreply.github.com>
2021-11-03 16:28:05 +0100

    Update run.py

--- mbdyn/run.py
+++ mbdyn/run.py
@@ -9,43 +9,28 @@ class MbdynAdapter:
 
 		while interface.is_coupling_ongoing():
 			if interface.is_action_required(precice.action_write_iteration_checkpoint()):
-				print("DUMMY: Writing iteration checkpoint")
 				interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
 			self.mbdyn.set_forces(np.vstack(self.force_tensor))
 			if self.mbdyn.solve(False):
 				self.module_logger.debug('Something went wrong!')
-				#MBDyn diverges
-				break
-			
-			##############################
-			### evaluate OpenFOAM data ###
-			##############################
-			
-			#############################
-			### evaluate preCICE data ###
-			#############################
+				exit("diverged")
+				
+			### evaluate OpenFOAM data
+
+			### evaluate preCICE data
 			
 			if interface.is_write_data_required(dt):
 				for i in range(self.patches):
 					interface.write_block_vector_data(self.write_data_id[i], self.vertex_ids[i], self.write_data[i])
 			
-			print("DUMMY: Advancing in time")
 			dt = interface.advance(dt)
 			self.current_time_step += decimal.Decimal(str(dt))
 			if interface.is_action_required(precice.action_read_iteration_checkpoint()):
-				print("DUMMY: Reading iteration checkpoint")
-				interface.mark_action_fulfilled(
-				    precice.action_read_iteration_checkpoint())
-				exit("ERROR MBDyn got no values")
+				exit("MBDyn is not in tight coupling mode!")
 			else:
 				
 				#MBDyn advance
 				if self.mbdyn.solve(True):
-					print("diverged")
-					break
-
-			self.iteration += 1
-		print("preCICE finalizing")
+					exit("diverged")
 		interface.finalize()
-		print("DUMMY: Closing python solver dummy...")
