9d4be42b6a46d6c6e6437554b9a6a6f5e831ea19
JulianSchl <83708903+JulianSchl@users.noreply.github.com>
2021-11-03 16:23:24 +0100

    Update run.py

--- mbdyn/run.py
+++ mbdyn/run.py
@@ -12,18 +12,6 @@ class MbdynAdapter:
 				print("DUMMY: Writing iteration checkpoint")
 				interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
-			for i in range(self.patches):
-				if interface.is_read_data_available():
-					self.read_data.append(interface.read_block_vector_data(self.read_data_id[i], self.vertex_ids[i]))
-				elif float(self.current_time_step) == float(decimal.Decimal(str(dt))):
-					self.read_data.append(self.vertices[i]*0)
-				else:
-					exit("no data provided!")
-				self.force_tensor.append(np.split(self.read_data[i],2)[0]*0) # prepare force_tensor for entries
-				self.force_tensor[i] = np.concatenate((self.force_tensor[i],self.force_tensor[i][:2,:]*0)) # prepare force_tensor for entries, add empty anchor & ground force node
-				for k,j in enumerate(np.split(self.read_data[i],2)):
-					self.force_tensor[i] += np.concatenate((j,j[:2,:]*0)) # fill values into force_tensor, add empty anchor & ground force node
-					
 			self.mbdyn.set_forces(np.vstack(self.force_tensor))
 			if self.mbdyn.solve(False):
 				self.module_logger.debug('Something went wrong!')
