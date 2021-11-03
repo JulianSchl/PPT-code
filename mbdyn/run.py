f3c43eb0f05ac2b9855b1273eba737eb80998937
JulianSchl <83708903+JulianSchl@users.noreply.github.com>
2021-11-03 16:28:36 +0100

    Update run.py

--- mbdyn/run.py
+++ mbdyn/run.py
@@ -22,7 +22,8 @@ class MbdynAdapter:
 			
 			if interface.is_write_data_required(dt):
 				for i in range(self.patches):
-					interface.write_block_vector_data(self.write_data_id[i], self.vertex_ids[i], self.write_data[i])
+					interface.write_block_vector_data(
+						self.write_data_id[i], self.vertex_ids[i], self.write_data[i])
 			
 			dt = interface.advance(dt)
 			self.current_time_step += decimal.Decimal(str(dt))
